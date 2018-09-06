47f382135b452de25353faa18bb09e2893c67004
akhiltripathi07071999 <42205097+akhiltripathi07071999@users.noreply.github.com>
2018-09-06 21:20:17 +0530

    Add files via upload

---
+++ akhil1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int n,i,j;
+ cout<<"enter the size of the pattern"<<endl;
+ cin>>n;
+ for(int j=0; j < n ; j++){
+    for (int i=0; i < n ; i++){
+        cout << "*";
+    }
+  cout << endl;
+    }
+  return 0;
+ }
+
